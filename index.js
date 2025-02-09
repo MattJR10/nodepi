fc2331fe3ef27703b267f129a0dc643f18194c4d
MattJR10 <114875622+MattJR10@users.noreply.github.com>
2025-02-08 21:08:24 -0500

    Update index.js

--- index.js
+++ index.js
@@ -586,8 +586,9 @@ app.post('/login-account', (req, res) => {
 	});
 });
 
-server.listen(3000, () => {
-	console.log("server running at http://localhost:3000");
+server.listen(3000, '0.0.0.0', () => {
+  console.log('Server running at http://localhost:3000');
 });
+
 
 
